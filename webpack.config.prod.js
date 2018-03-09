0d498734c873c7ec7049a48771d576868528db06
Nick Avi <nick.avignone@gmail.com>
2018-03-09 15:31:19 -0500

    change build path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,7 +18,7 @@ const config = {
   entry: path.resolve(__dirname, 'src/index'),
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: path.resolve(__dirname, './dist/js'),
+    path: path.resolve(__dirname, './dist/build'),
     publicPath: '/',
     filename: '[name].js'
   },
